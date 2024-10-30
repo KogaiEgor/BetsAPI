8fb0cc1408c551e17a7afb25ebc437b57971c781
egor <kogayegor@gmail.com>
2024-10-30 20:24:07 +0300

    added logging

--- src/api/bets/utils.py
+++ src/api/bets/utils.py
@@ -1,22 +1,24 @@
 from datetime import datetime, timedelta
 
+from src.logger import logger
+
 
 async def match_bets(read_bets: list, bets: list):
-    print(len(read_bets))
-    print(len(bets))
+    logger.info(f"Read {len(read_bets)}, found in db {len(bets)}")
     result = []
     count = 0
+    logger.debug(f"Start matching bets")
     for bet in bets:
         t = bet.bet_datetime
         for read_bet in read_bets:
             t2 = datetime.fromisoformat(read_bet['time']) + timedelta(hours=2)
             if abs((t - t2).total_seconds()) <= 10:
+                bet_id = bet.id
+                read_bet['bet_id'] = bet_id
                 result.append(read_bet)
-                print(t)
-                print(t2)
-                print("Bets matched")
+                logger.debug("Bets matched")
                 count += 1
                 break
-    print(count)
+    logger.info(f"Total matched bets {count}/{len(bets)}")
     return result
 
