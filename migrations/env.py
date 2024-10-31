d8e99a2e0c47a8c176e919d74ecc889f870b068e
egor <kogayegor@gmail.com>
2024-10-31 12:45:40 +0300

    fix bug

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.db.database import Base, DATABASE_URL, TEST_DB
+from src.db.database import Base, DATABASE_URL
 from src.api.allbets import model
 from src.api.bets import model
 
