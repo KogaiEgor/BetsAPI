9835c948a3a9bf4bf2ba2ca38e6fe667b2867e2f
egor <kogayegor@gmail.com>
2024-11-04 12:29:36 +0300

    add  acc_id to all parsed bets

--- src/api/bets/utils.py
+++ src/api/bets/utils.py
@@ -10,10 +10,10 @@ async def match_bets(read_bets: list, bets: list, acc_id: int):
     for bet in bets:
         t = bet.bet_datetime
         for read_bet in read_bets:
+            read_bet['acc_id'] = acc_id
             t2 = datetime.fromisoformat(read_bet['time']) + timedelta(hours=2)
             if abs((t - t2).total_seconds()) <= 10:
                 read_bet['bet_id'] = bet.id
-                read_bet['acc_id'] = acc_id
                 logger.debug("Bets matched")
                 count += 1
                 break
